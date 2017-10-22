7f8dc278f41bd783f17b851f29446a8b613a93a5
takanorihozumi <t.h032766@gmail.com>
2017-10-23 00:33:28 +0900

    use scrollView1

--- Swift/Training/SNSCatchUp/SNSCatchUp/EditViewController.swift
+++ Swift/Training/SNSCatchUp/SNSCatchUp/EditViewController.swift
@@ -13,10 +13,40 @@ class EditViewController: UIViewController {
     @IBOutlet var editImageVIew: UIImageView!
     var willEdit :UIImage = UIImage()
     
+    @IBOutlet var sc: UIScrollView!
+    
+    //scrollView用
+    var uv = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        editImageVIew.image = willEdit
+        
+        //位置とサイズを決める
+        uv.frame = CGRect(x: 0, y: 418, width: self.view.frame.size.width*2, height: 128)
+        
+        //UIButtonを作成
+        for i in 0..<6{
+            let button:UIButton = UIButton()
+            //x軸はi*80都することでボタンを一定間隔ごとに配置していく
+            button.frame = CGRect(x: i*80, y: 0, width: 80, height: 80)
+            button.tag = i
+            //どこのメソッドを呼ぶか定義しておく
+            button.addTarget(self, action: #selector(tap/*これを呼ぶ*/), for: .touchUpInside/*タッチアップ時にメソッド発動*/)
+            let buttonImage:UIImage = UIImage(named: String(i) + ".jpg")!
+            button.setImage(buttonImage, for: UIControlState.normal)
+            uv.addSubview(button)
+        }
+        //scrollViewにUIViewを貼り付ける
+        sc.addSubview(uv)
+        ///scrollVieのサイズをUIViewのサイズに合わせておく（２倍のサイズにしているためスクロールする）
+        sc.contentSize = uv.bounds.size
+    }
+    
+    @objc func tap(sender:UIButton){
+        //tagの番号によってimageにフィルターを付けたい
+        editImageVIew.image = willEdit
     }
 
     override func didReceiveMemoryWarning() {
