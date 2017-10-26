97730c0c66830fa15382e7fbd32253291ad3b38f
takanorihozumi <t.h032766@gmail.com>
2017-10-26 23:12:18 +0900

    add filter image

--- Swift/Training/SNSCatchUp/SNSCatchUp/EditViewController.swift
+++ Swift/Training/SNSCatchUp/SNSCatchUp/EditViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreImage
 
 class EditViewController: UIViewController {
+    
+    //これを使うとフィルターの速度が速くなる
+    var ciContext = CIContext()
 
     @IBOutlet var editImageVIew: UIImageView!
     var willEdit :UIImage = UIImage()
@@ -21,6 +25,7 @@ class EditViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        editImageVIew.image = nil
         editImageVIew.image = willEdit
         
         //位置とサイズを決める
@@ -46,8 +51,122 @@ class EditViewController: UIViewController {
     
     @objc func tap(sender:UIButton){
         //tagの番号によってimageにフィルターを付けたい
-        editImageVIew.image = willEdit
+        if sender.tag == 0{
+         filter1()
+        }else if sender.tag == 1{
+            filter2()
+        }else if sender.tag == 2{
+            filter3()
+        }else if sender.tag == 3{
+            filter4()
+        }else if sender.tag == 4{
+            filter5()
+        }else if sender.tag == 5{
+            filter6()
+        }
     }
+    
+    func filter1(){
+        
+        // image が 元画像のUIImage
+        let ciImage:CIImage = CIImage(image:editImageVIew.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CISepiaTone")!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(0.8, forKey: "inputIntensity")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        
+        editImageVIew.image = image2
+        
+        
+    }
+    
+    func filter2(){
+        let ciImage:CIImage = CIImage(image:editImageVIew.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CIColorMonochrome")!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(CIColor(red: 0.75, green: 0.75, blue: 0.75), forKey: "inputColor")
+        ciFilter.setValue(1.0, forKey: "inputIntensity")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        editImageVIew.image = image2
+        
+    }
+    
+    
+    func filter3(){
+        let ciImage:CIImage = CIImage(image:editImageVIew.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CIFalseColor" )!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(CIColor(red: 0.44, green: 0.5, blue: 0.2), forKey: "inputColor0")
+        ciFilter.setValue(CIColor(red: 1, green: 0.92, blue: 0.50), forKey: "inputColor1")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        editImageVIew.image = image2
+        
+        
+        
+    }
+    
+    
+    func filter4(){
+        
+        let ciImage:CIImage = CIImage(image:editImageView.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CIColorControls" )!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(1.0, forKey: "inputSaturation")
+        ciFilter.setValue(0.5, forKey: "inputBrightness")
+        ciFilter.setValue(3.0, forKey: "inputContrast")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        editImageView.image = image2
+        
+    }
+    
+    
+    func filter5(){
+        
+        let ciImage:CIImage = CIImage(image:editImageView.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CIToneCurve" )!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(CIVector(x: 0.0, y: 0.0), forKey: "inputPoint0")
+        ciFilter.setValue(CIVector(x: 0.25, y: 0.1), forKey: "inputPoint1")
+        ciFilter.setValue(CIVector(x: 0.5, y: 0.5), forKey: "inputPoint2")
+        ciFilter.setValue(CIVector(x: 0.75, y: 0.9), forKey: "inputPoint3")
+        ciFilter.setValue(CIVector(x: 1.0, y: 1.0), forKey: "inputPoint4")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        editImageView.image = image2
+        
+    }
+    
+    func filter6(){
+        let ciImage:CIImage = CIImage(image:editImageView.image!)!;
+        let ciFilter:CIFilter = CIFilter(name: "CIHueAdjust" )!
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(3.14, forKey: "inputAngle")
+        self.ciContext = CIContext(options: nil)
+        let cgimg:CGImage = ciContext.createCGImage(ciFilter.outputImage!, from:ciFilter.outputImage!.extent)!
+        //image2に加工後のUIImage
+        let image2:UIImage = UIImage(cgImage: cgimg, scale: 1.0, orientation:UIImageOrientation.up)
+        editImageView.image = image2
+        
+        
+    }
+
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
