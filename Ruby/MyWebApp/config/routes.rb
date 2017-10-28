1df3cb72894be8e4cc79d37aee3a7f91f35ef3b2
takanorihozumi <t.h032766@gmail.com>
2017-10-28 13:40:57 +0900

    use device

--- Ruby/MyWebApp/config/routes.rb
+++ Ruby/MyWebApp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'#pagescontrollerのindexアクションを実行するurlを作成する
+  devise_for :users
+  root :to => 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
