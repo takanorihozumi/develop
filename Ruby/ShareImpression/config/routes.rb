849b10a0cb8bdf1aea502a66620599ca14769336
takanorihozumi <t.h032766@gmail.com>
2018-02-19 19:39:26 +0900

    create profile view

--- Ruby/ShareImpression/config/routes.rb
+++ Ruby/ShareImpression/config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  root 'pages#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+Rails.application.routes.draw do  
+  root :to => 'pages#index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :users, only: [:show]
 end
