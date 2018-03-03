ec13ea8503fec20e2f94cd7fd9a51b87fb6f91b6
takanorihozumi <t.h032766@gmail.com>
2018-03-03 10:17:47 +0900

    create impressions controller

--- Ruby/ShareImpression/db/schema.rb
+++ Ruby/ShareImpression/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220021234) do
+ActiveRecord::Schema.define(version: 20180220093836) do
+
+  create_table "impressions", force: :cascade do |t|
+    t.string "title"
+    t.integer "season"
+    t.integer "episode"
+    t.integer "evaluation"
+    t.boolean "netabare"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_impressions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
